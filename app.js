d376390659b3396962b4a05b023f16ec57bde564
combeysamuel <combeysamuel@gmail.com>
2021-08-25 09:56:06 -0400

    Update

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log(`running script..........`)
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
